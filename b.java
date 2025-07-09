8cc97ebaf2eae22a4f476ba4151b6ad95964bef3
priyansurout7751 <priyansurout547@gmail.com>
2025-07-10 00:54:46 +0530

    it is how to excess non primitive data type  in the method

---
+++ b.java
@@ -0,0 +1,15 @@
+public class b {
+    public static void m1(demo h)
+    {
+        System.out.println(h);
+    }
+    public static void main(String [] args){
+        b m=new b();
+        m.m1("priyansu");
+
+
+    }
+
+    }
+    
+
