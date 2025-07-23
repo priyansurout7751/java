1aa8dc9d4e2b9c886c9379316c1d5d7174ed886c
priyansurout7751 <priyansurout547@gmail.com>
2025-07-23 11:36:15 +0530

    add

---
+++ u.java
@@ -0,0 +1,50 @@
+// class y{
+//     public void m1(){
+//         System.out.println("hello world");
+//     }
+// }
+// class x extends  y{
+//     public static void m2() {
+//         System.out.println("hello hay");
+    
+//     }
+//     }
+    // class c{
+    //     public void m1(int...x,int...y){
+    //         System.out.println("hello world");
+    //         System.out.println(x[]0);
+    //         System.out.println(x[1]);
+    //     }
+    //     public static void main(String[] args) {
+    //         c a=new c();
+    //         a.m1(10,2);
+            
+    //     }
+        
+    //     }
+    // class e{
+    //     public void m1(String s){
+    //         System.out.println("hell panner");
+    //     }
+    // }
+    // class t extends e{
+    //     public void m1(Object s){
+    //         System.out.println("panner butter masala");
+    //     }
+    // }
+    // class b{
+
+    
+    
+    //     public static void main(String[] args) {
+    //         t a=new t();
+    // a.m1(new Object());
+            
+    //     }
+    // }
+    
+
+    
+    
+    
+
