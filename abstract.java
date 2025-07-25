e6b1b31f701bee0d42891d49331938efad57298c
priyansurout7751 <priyansurout547@gmail.com>
2025-07-25 18:00:21 +0530

    add abstract in java

--- abstract.java
+++ abstract.java
@@ -1,21 +1,16 @@
 abstract   class A{
-
- public void m2(){
+    abstract  void m1();
+}
+  class cat extends A{
+abstract  public void m2(){
         System.out.println("abstract class can not have its own object ");
+}
+         public static void main(String[] args) {
+       cat c=new cat();
+        c.m1();
      }
+      }  
 
 
 
-}
-class b extends A {
-     
-
-
-   
-    public static void main(String[] args) {
-       b c=new b();
-        c.m2();
-        
-    }
-}
 
