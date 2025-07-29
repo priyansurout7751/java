f9033a13c1fc9914a7d16a0faa5552eae79ccc4c
priyansurout7751 <priyansurout547@gmail.com>
2025-07-29 23:33:31 +0530

    added

---
+++ lower.java
@@ -0,0 +1,56 @@
+ 
+ import java.util.Scanner;
+ 
+ 
+ public class lower { 
+    public static void main(String [] args){
+        Scanner s=new Scanner(System.in);
+        System.out.println("enter the number");
+        int a=s.nextInt();
+        // for(int i=1;i<=a;i++){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*"+"  ");
+        //     }
+        //     System.out.println();
+        // }
+        //    for(int i=a;i>=1;i--){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*"+"  ");
+        //     }
+        //     System.out.println();
+        // }
+        //problem 3
+        //  for(int i=1;i<=2*a;i++){
+        //     int ran=i>a?2*a-i:i;
+        //     for(int j=1;j<=ran;j++){
+        //         System.out.print("*"+" ");
+        //     }
+        //     System.out.println("");
+        // }
+        //problem 5
+        // for(int i=1;i<=2*a;i++){
+        //     //first we need to print the star
+        //     //first we need to find out how many column we need to print
+        //     int rad=i>a?2*a-i:i;
+        //     int space=a-rad;
+        //     //to print the space exactly no of column is present in my column
+        //     for(int j=1;j<=space;j++){
+        //         System.out.print(" ");
+        //     }
+        //     //then after print the space then print star
+        //     for(int j=1;j<=rad;j++){
+        //         System.out.print("*"+" ");
+        //     }
+        //     System.out.println();
+        // }
+        
+    
+        
+      
+
+
+
+    }
+    
+    
+}
