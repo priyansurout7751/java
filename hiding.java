4d86327e5c5e53d4d99543357df58fe817381923
priyansurout7751 <priyansurout547@gmail.com>
2025-08-05 11:41:50 +0530

    add

---
+++ hiding.java
@@ -0,0 +1,18 @@
+public class hiding {
+    public static void main(hiding t){
+        System.out.println("this is of parent class of method hiding ");
+    }
+    public static void main(String[] args) {
+        hiding.main(null);
+    
+
+        
+    }
+    
+}
+class test extends hiding{
+    public static void main(hiding t){
+        System.out.println("this is my child class method hiding ");
+    }
+
+}
